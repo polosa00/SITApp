3f96797e17da0986f1bd19b821ad0245d7533a8c
polosa00 <i@apolochanin.ru>
2023-09-04 19:33:09 +0300

    Initial Commit

---
+++ ShotInTime/ShotInTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotInTimeApp.swift
+//  ShotInTime
+//
+//  Created by Александр Полочанин on 4.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ShotInTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
